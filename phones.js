799eb69112ec5c332b37ce377473964b2bccc5a8
Surya Annadurai <suryaannadurai1041999@gmail.com>
2024-11-03 20:28:52 +0530

    phone hunt was hunted

---
+++ phones.js
@@ -0,0 +1,467 @@
+let oppo12 = [
+  {
+    brand: "Oppo ",
+    phone_name: "Find X5",
+    slug: "oppo_find_x5-11378",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/oppo-find-x5.jpg",
+  },
+  {
+    brand: "Oppo ",
+    phone_name: "Find X5 Pro",
+    slug: "oppo_find_x5_pro-11236",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/oppo-find-x5-pro.jpg",
+  },
+  {
+    brand: "Oppo ",
+    phone_name: "Find X5 Lite",
+    slug: "oppo_find_x5_lite-11323",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/oppo-find-x5-lite.jpg",
+  },
+  {
+    brand: "Oppo ",
+    phone_name: "Pad",
+    slug: "oppo_pad-11322",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/oppo-pad.jpg",
+  },
+  {
+    brand: "Oppo ",
+    phone_name: "A76",
+    slug: "oppo_a76-11367",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/oppo-a36.jpg",
+  },
+  {
+    brand: "Oppo ",
+    phone_name: "Reno7 5G",
+    slug: "oppo_reno7_5g-11350",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/oppo-reno7.jpg",
+  },
+  {
+    brand: "Oppo ",
+    phone_name: "A96",
+    slug: "oppo_a96-11316",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/oppo-a96.jpg",
+  },
+  {
+    brand: "Oppo ",
+    phone_name: "Reno6 Lite",
+    slug: "oppo_reno6_lite-11196",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/oppo-reno6-lite.jpg",
+  },
+  {
+    brand: "Oppo ",
+    phone_name: "A36",
+    slug: "oppo_a36-11314",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/oppo-a36.jpg",
+  },
+  {
+    brand: "Oppo ",
+    phone_name: "A11s",
+    slug: "oppo_a11s-11288",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/oppo-a11s.jpg",
+  },
+  {
+    brand: "Oppo ",
+    phone_name: "Reno7 Pro 5G",
+    slug: "oppo_reno7_pro_5g-11190",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/oppo-reno7-pro-r.jpg",
+  },
+  {
+    brand: "Oppo ",
+    phone_name: "Find N",
+    slug: "oppo_find_n-11267",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/oppo-find-n.jpg",
+  },
+];
+
+let iphone12 = [
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 13 mini",
+    slug: "apple_iphone_13_mini-11104",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 13 Pro",
+    slug: "apple_iphone_13_pro-11102",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 13 Pro Max",
+    slug: "apple_iphone_13_pro_max-11089",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro-max.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 13",
+    slug: "apple_iphone_13-11103",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 12 Pro Max",
+    slug: "apple_iphone_12_pro_max-10237",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-pro-max-.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 12 Pro",
+    slug: "apple_iphone_12_pro-10508",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-pro--.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 12",
+    slug: "apple_iphone_12-10509",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 12 mini",
+    slug: "apple_iphone_12_mini-10510",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-mini.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone SE (2020)",
+    slug: "apple_iphone_se_(2020)-10170",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-se-2020.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 11 Pro Max",
+    slug: "apple_iphone_11_pro_max-9846",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-11-pro.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 11 Pro",
+    slug: "apple_iphone_11_pro-9847",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-11-pro-max-.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 11",
+    slug: "apple_iphone_11-9848",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-11.jpg",
+  },
+];
+
+let samsung12 = [
+  {
+    brand: "Samsung ",
+    phone_name: "Galaxy S22 5G",
+    slug: "samsung_galaxy_s22_5g-11253",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s22-5g.jpg",
+  },
+  {
+    brand: "Samsung ",
+    phone_name: "Galaxy Tab S8+",
+    slug: "samsung_galaxy_tab_s8+-11342",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-tab-s8-plus.jpg",
+  },
+  {
+    brand: "Samsung ",
+    phone_name: "Galaxy Tab S8 Ultra",
+    slug: "samsung_galaxy_tab_s8_ultra-11274",
+    image:
+      "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-tab-s8-ultra.jpg",
+  },
+  {
+    brand: "Samsung ",
+    phone_name: "Galaxy S22+ 5G",
+    slug: "samsung_galaxy_s22+_5g-11252",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s22-plus-5g.jpg",
+  },
+  {
+    brand: "Samsung ",
+    phone_name: "Galaxy Tab S8",
+    slug: "samsung_galaxy_tab_s8-11343",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-tab-s8.jpg",
+  },
+  {
+    brand: "Samsung ",
+    phone_name: "Galaxy S21 FE 5G",
+    slug: "samsung_galaxy_s21_fe_5g-10954",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s21-fe-5g.jpg",
+  },
+  {
+    brand: "Samsung ",
+    phone_name: "Galaxy S22 Ultra 5G",
+    slug: "samsung_galaxy_s22_ultra_5g-11251",
+    image:
+      "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s22-ultra-5g.jpg",
+  },
+  {
+    brand: "Samsung ",
+    phone_name: "Galaxy Tab A8 10.5 (2021)",
+    slug: "samsung_galaxy_tab_a8_10_5_(2021)-11265",
+    image:
+      "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-tab-a8-105-2021-new.jpg",
+  },
+  {
+    brand: "Samsung ",
+    phone_name: "Galaxy A13 5G",
+    slug: "samsung_galaxy_a13_5g-11149",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a13-5g-.jpg",
+  },
+  {
+    brand: "Samsung ",
+    phone_name: "Galaxy A03 Core",
+    slug: "samsung_galaxy_a03_core-11215",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a03-core.jpg",
+  },
+  {
+    brand: "Samsung ",
+    phone_name: "Galaxy A03",
+    slug: "samsung_galaxy_a03-11244",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a03.jpg",
+  },
+  {
+    brand: "Samsung ",
+    phone_name: "Galaxy F42 5G",
+    slug: "samsung_galaxy_f42_5g-11124",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-f42-5g.jpg",
+  },
+];
+
+let onloadPhones = [
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 13 mini",
+    slug: "apple_iphone_13_mini-11104",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 13 Pro",
+    slug: "apple_iphone_13_pro-11102",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 13 Pro Max",
+    slug: "apple_iphone_13_pro_max-11089",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro-max.jpg",
+  },
+  {
+    brand: "Apple ",
+    phone_name: "iPhone 13",
+    slug: "apple_iphone_13-11103",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13.jpg",
+  },
+  {
+    brand: "Samsung ",
+    phone_name: "Galaxy A13 5G",
+    slug: "samsung_galaxy_a13_5g-11149",
+    image: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a13-5g-.jpg",
+  },
+];
+
+let btn = document.querySelector(".search-btn");
+let input = document.querySelector(".input");
+let phoneSection = document.querySelector(".phones-section");
+let popUp = document.querySelector(".popUp");
+
+const apiUrl1 = "https://openapi.programming-hero.com/api/phones?search=oppo";
+const apiUrl2 = "https://openapi.programming-hero.com/api/phones?search=apple";
+const apiUrl3 =
+  "https://openapi.programming-hero.com/api/phones?search=samsung";
+
+async function demo() {
+  let promise1 = await fetch(apiUrl1)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+
+  // console.log(typeof(promise1))
+
+  let promise2 = await fetch(apiUrl2)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+
+  // console.log(promise2.data)
+
+  let promise3 = await fetch(apiUrl3)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+
+  // console.log(promise3.data)
+
+  btn.addEventListener("click", (event) => {
+    //  showAll.style.visibility = "visible"
+    if (input.value.toLowerCase() == "oppo") {
+      displayfewphones(oppo12, promise1);
+    } else if (input.value.toLowerCase() == "iphone") {
+      displayfewphones(iphone12, promise2);
+    } else if (input.value.toLowerCase() == "samsung") {
+      displayfewphones(samsung12, promise3);
+    } else {
+      alert("Search either Oppo or Samsung or Iphone");
+    }
+  });
+}
+
+demo();
+
+window.onload = () => {
+  displayfewphones(onloadPhones);
+};
+
+function displayfewphones(pro, promise) {
+  let showData = "";
+
+  pro.forEach((value, index) => {
+    //   // console.log(value);
+
+    showData += `
+        <div class="phone-box">
+            <div class="phones">
+                <img src=${value.image}>
+                <h2> ${value.phone_name}</h2>
+                <p>There are many variations of passages of available, <br> but the majority have suffered</p>
+                <button class="signup">SHOW DETAILS</button>
+            </div>
+        </div> 
+    `;
+    phoneSection.innerHTML = showData;
+
+    activateBtns2(pro);
+  });
+
+  let showAll = document.createElement("button");
+  showAll.classList.add("signup");
+  showAll.innerText = "SHOW ALL";
+  phoneSection.appendChild(showAll);
+  console.log(showData);
+  showAll.addEventListener("click", () => {
+    displayPhones(promise);
+  });
+}
+
+function displayPhones(promise) {
+  let showData = "";
+  promise.data.forEach((value, index) => {
+    //   // console.log(value);
+
+    showData += `
+        <div class="phone-box">
+            <div class="phones">
+                <img src=${value.image}>
+                <h2> ${value.phone_name}</h2>
+                <p>There are many variations of passages of available, <br> but the majority have suffered</p>
+                <button class="signup">SHOW DETAILS</button>
+            </div>
+        </div> 
+    `;
+    phoneSection.innerHTML = showData;
+
+    activateBtns(promise);
+  });
+
+  //  console.log(showData);
+}
+
+function activateBtns2(pro) {
+  let container = document.querySelectorAll(".phone-box");
+  //  console.log(container)
+  container.forEach((value, index) => {
+    value.addEventListener("click", (event) => {
+      if (event.target.innerText == "SHOW DETAILS") {
+        popUp.addEventListener("click", (event) => {
+          if (event.target.classList.contains("close")) {
+            popUp.style.visibility = "hidden";
+          }
+        });
+
+        popUp.innerHTML = `
+            <div class="container">
+           <img src="${pro[index].image}" alt="">
+           <h2>${pro[index].phone_name}</h2>
+           <p><strong>Storage</strong> : 256GB storage, microSDXC
+               displaySize: 6.43 inches, 99.8 cm2 (~84.9% screen-to-body ratio)
+           </p><p><strong>ChipSet</strong> :  MediaTek MT6877 Dimensity 900 5G (6 nm)
+               memory: 256GB 8GB RAM
+               sensors: Fingerprint (under display,optical),accelerometer,gyro,proximity,compass
+               
+               Released 2022, February 17
+           </p>
+           <button class="close">CLOSE</button>
+       </div>
+          `;
+        popUp.style.visibility = "visible";
+
+        console.log(index);
+      }
+    });
+  });
+
+  //  console.log(container);
+}
+
+function activateBtns(promise) {
+  let container = document.querySelectorAll(".phone-box");
+  //  console.log(container)
+  container.forEach((value, index) => {
+    value.addEventListener("click", (event) => {
+      if (event.target.innerText == "SHOW DETAILS") {
+        popUp.addEventListener("click", (event) => {
+          if (event.target.classList.contains("close")) {
+            popUp.style.visibility = "hidden";
+          }
+        });
+
+        popUp.innerHTML = `
+             <div class="container">
+            <img src="${promise.data[index].image}" alt="">
+            <h2>${promise.data[index].phone_name}</h2>
+            <p><strong>Storage</strong> : 256GB storage, microSDXC
+                displaySize: 6.43 inches, 99.8 cm2 (~84.9% screen-to-body ratio)
+            </p><p><strong>ChipSet</strong> :  MediaTek MT6877 Dimensity 900 5G (6 nm)
+                memory: 256GB 8GB RAM
+                sensors: Fingerprint (under display,optical),accelerometer,gyro,proximity,compass
+                
+                Released 2022, February 17
+            </p>
+            <button class="close">CLOSE</button>
+        </div>
+           `;
+        popUp.style.visibility = "visible";
+
+        console.log(index);
+      }
+    });
+  });
+
+  //  console.log(container);
+}
